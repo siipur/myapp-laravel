b963ab03c8f5fdf94476bac01e130a28b48ec733
siipur <ghopurnomo@gmail.com>
2019-02-01 04:12:57 +0700

    memahami basic routing

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,26 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+/*
+Route::get ('/about', function (){
+    return view('pages.about'); //buatlah file about.blade.php pada view/pages  
+});
+Route::get('/hai', function () {
+    return "<h1>Hello Bro!</h1>";
+});
+// Contoh Dinamic Route
+Route::get ('/user/{id}/{name}', function ($id,$name){
+    return 'ini user '.$name.' dengan nomor id '.$id;   
+});
+
+
+Route::get ('/about', function (){
+    return view('pages.about'); //buatlah file about.blade.php pada view/pages  
+});
+*/
+
+// route ke controller (rekomendasi)
+Route::get ('/', 'PagesController@index');
+Route::get ('/about', 'PagesController@about');
+Route::get ('/services', 'PagesController@services');
