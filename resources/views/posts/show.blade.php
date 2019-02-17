b4e0d57f623b3ade253cfe5efaed0ed553ad44f6
siipur <ghopurnomo@gmail.com>
2019-02-18 06:20:14 +0700

    Learning Delete Request

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -8,15 +8,9 @@
     <hr/>
     <small>Written on {{$post->created_at}}</small>
     <hr/>
-    <a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a>
-
+    <a href="/posts/{{$post->id}}/edit" class="btn btn-primary">Edit</a>
     {!! Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right']) !!}
         {{ Form::hidden('_method', 'DELETE') }}
         {{ Form::submit('Delete', ['class' => 'btn btn-danger']) }}
-    {!! Form::close() !!}
-    
-    <!--
-    <a href="/posts/{{$post->id}}/delete" class="btn btn-danger">Hapus</a>
-    -->
-
+    {!! Form::close() !!}  
 @endsection
